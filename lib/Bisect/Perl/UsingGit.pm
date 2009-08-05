da1cc85ab32f013f7236516f6d5f419452cb9ac8
Leon Brocard <acme@astray.com>
2009-08-05 10:40:04 +0100

    move around to a module

---
+++ lib/Bisect/Perl/UsingGit.pm
@@ -0,0 +1,135 @@
+package Bisect::Perl::UsingGit;
+use Moose;
+with 'MooseX::Getopt';
+use Capture::Tiny qw(tee);
+
+has 'action'   => ( is => 'rw', isa => 'Str',  required => 1 );
+has 'filename' => ( is => 'rw', isa => 'Str',  required => 1 );
+has 'verbose'  => ( is => 'rw', isa => 'Bool', default  => 0 );
+
+sub run {
+    my $self   = shift;
+    my $action = $self->action;
+    return $self->$action;
+}
+
+sub file_added {
+    my $self = shift;
+    my $filename = $self->filename;
+    $self->_describe();
+
+    if ( -f $filename ) {
+        $self->_message("have $filename");
+        exit 1;
+    } else {
+        $self->_message("do not have $filename");
+        exit 0;
+    }
+}
+
+sub file_removed {
+    my $self = shift;
+    my $filename = $self->filename;
+    $self->_describe();
+
+    unless ( -f $filename ) {
+        $self->_message("have $filename");
+        exit 1;
+    } else {
+        $self->_message("do not have $filename");
+        exit 0;
+    }
+}
+
+sub perl_fails {
+    my $self = shift;
+        my $filename = $self->filename;
+
+    $self->_describe();
+
+    $self->_call_or_error('git clean -dxf');
+
+    # Fix configure error in makedepend: unterminated quoted string
+    # http://perl5.git.perl.org/perl.git/commitdiff/a9ff62
+    $self->_call_or_error(q{perl -pi -e "s|##\`\"|##'\`\"|" makedepend.SH}) if -f 'makedepend.SH';
+
+    # Allow recent gccs (4.2.0 20060715 onwards) to build perl.
+    # It switched from '<command line>' to '<command-line>'.
+    # http://perl5.git.perl.org/perl.git/commit/d64920
+    $self->_call_or_error(
+        q{perl -pi -e "s|command line|command-line|" makedepend.SH}) if -f 'makedepend.SH';
+
+    # http://perl5.git.perl.org/perl.git/commit/205bd5
+    $self->_call_or_error(
+        q{perl -pi -e "s|#   include <asm/page.h>||" ext/IPC/SysV/SysV.xs}) if -f 'ext/IPC/SysV/SysV.xs';
+
+    $self->_call_or_error(
+        'sh Configure -des -Dusedevel -Doptimize="-g" -Dcc=ccache\ gcc -Dld=gcc'
+    );
+
+    -f 'config.sh' || $self->_error('Missing config.sh');
+
+    $self->_call_or_error('make');
+    -x './perl' || $self->_error('No ./perl executable');
+
+    my $code = $self->_call("./perl -Ilib $filename")->{code};
+    $self->_message("Status: $code");
+    if ( $code < 0 || $code >= 128 ) {
+        $self->_message("Changing code to 127 as it is < 0 or >= 128");
+        $code = 127;
+    }
+
+    $self->_call_or_error('git clean -dxf');
+    $self->_call_or_error('git checkout ext/IPC/SysV/SysV.xs') if -f 'ext/IPC/SysV/SysV.xs';
+    $self->_call_or_error('git checkout makedepend.SH') if -f 'makedepend.SH';
+
+    exit $code;
+}
+
+
+sub _describe {
+    my $self = shift;
+    my $describe = $self->_call_or_error('git describe')->{stdout};
+    chomp $describe;
+    $self->_error('No git describe') unless $describe;
+    $self->_message("\n*** $describe ***\n");
+}
+
+sub _call {
+    my($self, $command) = @_;
+    $self->_message('calling $command');
+    my $status;
+    my ( $stdout, $stderr ) = tee {
+        $status = system($command);
+    };
+    my $code = $status >> 8;
+    return {
+        code   => $code,
+        stdout => $stdout,
+        stderr => $stderr,
+    };
+}
+
+sub _call_or_error {
+    my($self, $command)  = @_;
+    my $captured = $self->_call($command);
+    unless ( $captured->{code} == 0 ) {
+        $self->_error( "$command failed: $?: " . $captured->{stderr} );
+    }
+    $self->_message($command);
+    return $captured;
+}
+
+sub _message {
+    my($self, $text) = @_;
+#    $log->print("$text\n");
+    print "$text\n";
+}
+
+sub _error {
+    my($self, $text) = @_;
+    $self->_message($text);
+    exit 125;
+}
+
+1;
